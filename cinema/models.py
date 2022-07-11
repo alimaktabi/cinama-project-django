a4e5eb801d3bc3e8de10550b838dd9e9ee4f7646
daniyalizadpanahi <107469397+daniyalizadpanahi@users.noreply.github.com>
2022-07-11 20:03:48 +0430

    made models (cinema, ticket and sans)

--- cinema/models.py
+++ cinema/models.py
@@ -1,8 +1,33 @@
 from django.db import models
 
-class cinema(models.Model):
-    name = models.CharField(max = 100)
-    city =
-    capacity
-    adress
-    about
+
+class Cinema(models.Model):
+    name = models.CharField(max_length=100)
+    city = models.CharField(max_length=50)
+    province = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+    address = models.TextField()
+    about = models.TextField()
+
+
+class Movie(models.Model):
+    Name = models.CharField(max_length=50)
+    length = models.IntegerField()
+    year = models.IntegerField()
+    director = models.CharField(max_length=50)
+    genre = models.CharField(max_length=30)
+    about = models.TextField()
+
+
+class Sans(models.Model):
+    movie_name = models.Foreignkey(Movie, on_delete=models.CASCADE)
+    start_time = models.Datetime
+    price = models.IntegerField
+
+
+class Ticket(models.Model):
+    name = models.Foreignkey(Movie , on_delete=models.PROTECT)
+    movie = models.Foreignkey(Sans , on_delete=models.CASCADE)
+    count = models.IntegerField
+    seat = models.IntegerField
+
