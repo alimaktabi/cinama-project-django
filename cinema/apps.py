e0ec7f9d78ed87f14c4b684546a9d5905d8c5d16
daniyalizadpanahi <107469397+daniyalizadpanahi@users.noreply.github.com>
2022-07-20 22:18:49 +0430

    ...

--- cinema/apps.py
+++ cinema/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
 
 
+# This is identification of the app
+
 class CinemaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'cinema'
--- cinema/models.py
+++ cinema/models.py
@@ -1,3 +1,5 @@
+import random
+
 from django.db import models
 
 
@@ -10,27 +12,29 @@ class Cinema(models.Model):
     about = models.TextField()
 
     def __str__(self):
-        return self
+        return self.name
 
 
 class Movie(models.Model):
     Name = models.CharField(max_length=50)
-    length = models.IntegerField()
-    year = models.IntegerField()
+    length = models.IntegerField(blank=True, null=True)
+    year = models.IntegerField(blank=True, null=True)
     director = models.CharField(max_length=50)
     genre = models.CharField(max_length=30)
     about = models.TextField()
 
+    def __str__(self):
+        return self.Name
+
 
 class Sans(models.Model):
     movie_name = models.ForeignKey('Movie', on_delete=models.CASCADE)
-    start_time = models.DateTimeField
-    price = models.IntegerField
+    start_time = models.DateTimeField(default=)
+    price = models.IntegerField(default=10000)
 
 
 class Ticket(models.Model):
-    name = models.ForeignKey('Movie' , on_delete=models.PROTECT)
-    movie = models.ForeignKey('Sans' , on_delete=models.CASCADE)
-    count = models.IntegerField
-    seat = models.IntegerField
-
+    name = models.ForeignKey('Movie', on_delete=models.PROTECT)
+    movie = models.ForeignKey('Sans', on_delete=models.CASCADE)
+    count = models.IntegerField(default=1)
+    seat = models.IntegerField(default=0)
